a9f7518275902abd180e663f8fe2f612a408d747
TerryRoberts1995 <terryjbroberts@yahoo.com>
2020-08-25 21:02:00 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRoutes } from 'hookrouter';
+import FridgeNav from './components/navigation/FridgeNav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This component is working just fine.</h1>
+
+      <FridgeNav />
     </div>
   );
 }
---
+++ src/components/navigation/FridgeNav.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+
+export default function FridgeNav() {
+
+    return <div>This is the fridge Navigation.</div>
+}
